afdb15df51aa2487e4edaa057d580e4ebdbb2f59
Devam Patel <devampatelcode@gmail.com>
2024-01-09 23:59:51 +0530

    added movie db api link and key

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//api key :bc848061
+const API_URL = 'http://www.omdbapi.com?apikey=bc848061';
+
+const App=() =>{
+    const searchmovies = async (title)=>{
+        const response = await fetch (`${API_URL}&s=${title}`);
+        const data =await response.json();
+        console.log(data);
+
+
+    }
+    useEffect(()=>{
+        searchmovies('batman');
+
+    },[])
+
+    return (
+        <><div className="Search">
+            <h1>POPCORN TIME</h1>
+            <input
+                placeholder="Search a movie"
+                value="spiderman"
+                type="text"
+                onChange={() => { } } />
+            <img class="search_icon"
+                src=""
+                onClick={() => { } } />
+        </div>
+        
+        <div className="container">
+            
+            
+        </div></>
+    )
+    
 }
 
 export default App;
