9429ab400b025421933da767be531d3ef7cccaf3
Devam Patel <devampatelcode@gmail.com>
2024-01-10 15:42:11 +0530

    movie card component added
    movie card and react side , movie search and result display in card format

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState,useEffect } from "react";
+import Movie_card from "./card";
+import "./App.css";
+
 
 //api key :bc848061
 const API_URL = 'http://www.omdbapi.com?apikey=bc848061';
 
 const App=() =>{
+
+    const [searchkeyword,setsearchkeyword]=useState('');
+    const [ list_of_movies,setlist_of_movies]=useState([]);
+
     const searchmovies = async (title)=>{
         const response = await fetch (`${API_URL}&s=${title}`);
         const data =await response.json();
-        console.log(data);
+        setlist_of_movies(data.Search);
 
 
     }
@@ -17,25 +24,41 @@ const App=() =>{
 
     },[])
 
+   
+
     return (
-        <><div className="Search">
-            <h1>POPCORN TIME</h1>
-            <input
-                placeholder="Search a movie"
-                value="spiderman"
-                type="text"
-                onChange={() => { } } />
-            <img class="search_icon"
-                src=""
-                onClick={() => { } } />
-        </div>
+        <div className="app">
+            <h1 className="heading">POPCORN TIME</h1>
+            <div className="search">
+                <input
+                    placeholder="Search a movie"
+                    value={searchkeyword}
+                    type="text"
+                    onChange={(e) => setsearchkeyword(e.target.value)} />
+                <img className="search_icon"
+                    src="search.svg"
+                    onClick={() => searchmovies(searchkeyword)} 
+                    />
+            </div>
         
+        
+        {list_of_movies?.length > 0 ? (
         <div className="container">
-            
-            
-        </div></>
-    )
+            {list_of_movies.map((each_movie)=>(
+                <Movie_card movie={each_movie}/>
+            ))}
+
+
+        </div>
+        ) :(
+            <div className="notfound">
+                <h2>No Movies Found</h2>
+            </div>
+        )}
+        </div>
+    );
+
     
-}
+};
 
 export default App;
